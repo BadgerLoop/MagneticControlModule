87bff451550603c083204dc0819c20d929b34293
Nicholas Jaunich <njaunich@10.140.23.10>
2016-05-24 21:24:46 -0500

    Initial Commit

--- main.h
+++ main.h
@@ -11,3 +11,10 @@ char message[MAX_LENGTH];
 
 #define SYSCLK      (64000000ul)    // Hz
 #define PBCLK       (64000000ul)
+
+#define SID1    0x146       // the SID that this module responds to
+#define SID2    0x146    
+
+#define FIFO_0_SIZE     4   // RX, in # message buffers
+#define FIFO_1_SIZE     2   // TX, in # message buffers
+#define MB_SIZE         4   //
